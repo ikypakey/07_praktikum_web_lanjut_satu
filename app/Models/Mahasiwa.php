920d225de43dec8cc59022d3f05f68ace27ce4b0
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-28 12:15:35 +0700

    edit on madel mahasiswa

--- app/Models/Mahasiwa.php
+++ app/Models/Mahasiwa.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;  
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiwa as Authenticatable;  
+use Illuminate\Notifications\Notifiable; 
+use Illuminate\Database\Eloquent\Model; //Model Eloquent 
 
-class Mahasiwa extends Model
+class Mahasiwa extends Model //Definisi Model
+
 {
-    use HasFactory;
-}
+    protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = 'id_mahasiswa'; // Memanggil isi DB Dengan primarykey
+    
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+    'Nim',
+    'Nama',
+    'Kelas',
+    'Jurusan',
+    ];
+};
