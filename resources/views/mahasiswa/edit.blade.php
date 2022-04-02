4ae7ba48c481c07e9a17ce1f72b8e3bd8e6b9469
masbarok193@gmail.com <masbarok193@gmail.com>
2022-04-02 10:23:27 +0700

    add value on detail and edit.blade

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -42,15 +42,15 @@
                     </div>
                     <div class="form-group">
                         <label for="Jurusan">Email</label>
-                        <input type="varchar" name="email" class="form-control" id="Email">
+                        <input type="varchar" name="email" class="form-control" value="{{ $Mahasiswa->email }}"  id="Email">
                     </div>
                     <div class="form-group">
                         <label for="Jurusan">Alamat</label>
-                        <input type="varchar" name="alamat" class="form-control" id="Alamat">
+                        <input type="varchar" name="alamat" value="{{ $Mahasiswa->alamat }}"  class="form-control" id="Alamat">
                     </div>
                     <div class="form-group">
                         <label for="Jurusan">Tanggal Lahir</label>
-                        <input type="date" name="tanggal_lahir" class="form-control" id="TanggalLahir">
+                        <input type="date" name="tanggal_lahir" class="form-control" value="{{ $Mahasiswa->tanggal_lahir }}"  id="TanggalLahir">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
