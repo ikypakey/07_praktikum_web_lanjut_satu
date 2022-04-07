28081a2a5842573a02065f411ba9d3b80d04658c
masbarok193@gmail.com <masbarok193@gmail.com>
2022-04-07 23:11:22 +0700

    edit kelas model to define class has many mahasiswa

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiwa;
 
 class Kelas extends Model
 {
     use HasFactory;
+    protected $table='kelas'; //mendefinisikan bahwa model ini terkait dengan tabel kelas
+
+    public function mahasiswa(){
+        return $this->hasMany(Mahasiswa::class);
+    }
 }
